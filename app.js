983c418f4b0d85db3c97d714798ce2af5e6c88f4
ping58972 <ping58972@gmail.com>
2019-05-28 23:52:58 -0500

    clean up for ejs

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-//const expressHbs = require('express-handlebars');
 const app= express();
 
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
-
-
 app.set('view engine', 'ejs');
-// app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-// app.set('view engine', 'handlebars');
-// app.engine('handlebars', expressHbs());
-// app.set('view engine', 'handlebars');
-// app.set('view engine', 'pug');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -22,9 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', shopRoute);
 app.use('/admin', adminRoute.router);
-
 app.use('/', (req, res, next)=>{
-    //res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
     res.render('404', {docTitle: '404 Page', path: ''});
 });
 
